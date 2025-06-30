0d236c652a230a3fb05ebe7816bf5245c1d98dcd
sadrisso <sadriisso304@gmail.com>
2025-06-30 20:15:36 +0600

    access modifiers shorthand practice

--- class.js
+++ class.js
@@ -1,17 +1,18 @@
-var Player = /** @class */ (function () {
-    function Player(n, a, c) {
-        this.name = n;
-        this.age = a;
-        this.country = c;
+"use strict";
+class Player {
+    constructor(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
     }
-    Player.prototype.play = function () {
-        console.log("".concat(this.name, " is playing for ").concat(this.country, " and his age is ").concat(this.age));
-    };
-    return Player;
-}());
-var mashrafi = new Player("Mashrafi", 40, "Bangladesh");
-var sakib = new Player("Sakib", 33, "BD");
-var players = [];
+    play() {
+        console.log(`${this.name} is playing for ${this.country} and his age is ${this.age}`);
+    }
+}
+const mashrafi = new Player("Mashrafi", 40, "Bangladesh");
+const sakib = new Player("Sakib", 33, "BD");
+const players = [];
 players.push(mashrafi);
 players.push(sakib);
-console.log();
+console.log(sakib);
+console.log("This is players array: ", players);
--- class.ts
+++ class.ts
@@ -1,17 +1,15 @@
 class Player {
-    name: string
-    age: number
-    country: string
+  constructor(
+    private name: string,
+    public age: number,
+    readonly country: string
+  ) {}
 
-    constructor(n: string, a: number, c: string) {
-        this.name = n
-        this.age = a
-        this.country = c
-    }
-
-    play () {
-        console.log(`${this.name} is playing for ${this.country} and his age is ${this.age}`)
-    }
+  play() {
+    console.log(
+      `${this.name} is playing for ${this.country} and his age is ${this.age}`
+    );
+  }
 }
 
 const mashrafi = new Player("Mashrafi", 40, "Bangladesh");
@@ -19,7 +17,8 @@ const sakib = new Player("Sakib", 33, "BD");
 
 const players: Player[] = [];
 
-players.push(mashrafi)
-players.push(sakib)
+players.push(mashrafi);
+players.push(sakib);
 
-console.log(sakib)
+console.log(sakib);
+console.log("This is players array: ", players)
